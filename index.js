ec4c24d519a670148cf4d7acbaeefb0832928728
Candace Wilson <candaceyw@gmail.com>
2020-04-19 20:17:24 -0400

    MVP complete

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express');
+const actions = require('./routes/ActionRouter');
+const projects = require('./routes/ProjectRouter');
+const cors = require('cors');
+const server = express();
+const port = 4500;
 
-Sing along:
+server.use(express.json());
+server.use(cors());
+server.use(logger);
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+server.use('/projects', projects);
+server.use('/actions', actions);
 
-Go code!
-*/
+server.get('/', (req, res) => {
+	res.status(200).json({ message: 'Success' });
+});
+
+function logger(req, res, next) {
+	console.log(`${req.method} ${req.url} ${new Date().toISOString()}`);
+	next();
+}
+
+server.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
---
+++ routes/ProjectRouter.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const projects = require('../data/helpers/projectModel');
+
+// GET return all projects
+router.get('/', async (req, res, next) => {
+	try {
+		const data = await projects.get();
+		res.status(200).json(data);
+	} catch (error) {
+		next(error);
+	}
+});
+//working
+
+// GET return project by Id
+router.get('/:id', async (req, res, next) => {
+	const id = req.params.id;
+	try {
+		const data = await projects.get(id);
+		res.status(200).json(data);
+	} catch (error) {
+		next(error);
+	}
+});
+//working
+
+// POST insert new project
+router.post('/', async (req, res, next) => {
+	try {
+		const data = await projects.insert(req.body);
+		res.status(201).json(data);
+	} catch (error) {
+		next(error);
+	}
+});
+//working
+
+// PUT update project by id
+router.put('/:id', async (req, res, next) => {
+	try {
+		const data = await projects.update(req.params.id, req.body);
+		res.status(200).json(data);
+	} catch (error) {
+		next(error);
+	}
+});
+//working
+
+// DELETE remove project by id
+router.delete('/:id', async (req, res, next) => {
+	try {
+		await projects.remove(req.params.id);
+		res.status(204).end();
+	} catch (error) {
+		next(error);
+	}
+});
+//working
+
+module.exports = router;
